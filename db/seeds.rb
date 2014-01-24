52530379610e766f0c201576b9c4515a22e38100
Dom Siravo <domsiravo@gmail.com>
2014-01-24 01:07:16 +0000

    show view and action for articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,9 @@
 categories = Category.create([
                 { name: 'History'}, {name: 'Biology'}, {name: 'Literature'},
                 { name: 'Mathematics'}, { name: 'Music Theory'}, { name: 'Computer Science'},
-                { name: 'Sociology'}, {name: 'Chemistry'}])
+                { name: 'Sociology'}, {name: 'Chemistry'}
+        ])
 
-# create 50 articles, with random titles, 250 words of content, and
-# randomly assign one of the categories above to each article
 for i in 0..49
         title = Faker::Lorem.sentence(rand(2..10)).chomp('.')
         content = Faker::Lorem.paragraph(word_count=250)
